97fae539d01f3c1226e7cccb3de3fdcbbdfd34e6
Badut Bego <147524804+badutbego@users.noreply.github.com>
2023-11-08 03:00:06 +0000

    Wah nilai 100 nih uts kayaknya

--- uts.py
+++ uts.py
@@ -3,3 +3,16 @@
 # Copyright (c) Pipin Fitriadi. All rights reserved.
 # Licensed under the MIT License. See LICENSE in the project root for license
 # information.
+
+class Sepeda:
+    total_roda: int
+
+    def __init__(self, total_roda: int):
+        self.total_roda = total_roda
+
+    def get_total_roda(self) -> int:
+        return self.total_roda
+
+
+s = Sepeda(3)
+print(s.get_total_roda())
