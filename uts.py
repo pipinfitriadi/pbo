4115db648b9b57cf2bd190e6962a0d3a9cb73222
Bagus Santos <147573283+Bagusantos@users.noreply.github.com>
2023-11-15 01:47:30 +0000

    Jawaban UTS Bagus Santoso

--- uts.py
+++ uts.py
@@ -1,5 +1,21 @@
-#!/usr/bin/env python3
+class Lingkaran:
+    def __init__(self, jari_jari):
+        self.jari_jari = jari_jari
 
-# Copyright (c) Pipin Fitriadi. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license
-# information.
+    def hitung_luas(self):
+        luas = 3.14 * self.jari_jari**2
+        return luas
+
+    def hitung_keliling(self):
+        keliling = 2 * 3.14 * self.jari_jari
+        return keliling
+
+
+jari_jari_lingkaran = 10
+lingkaran_saya = Lingkaran(jari_jari_lingkaran)
+
+luas_lingkaran = lingkaran_saya.hitung_luas()
+keliling_lingkaran = lingkaran_saya.hitung_keliling()
+
+print("Luas lingkaran: ", luas_lingkaran)
+print("Keliling lingkaran: ", keliling_lingkaran)
