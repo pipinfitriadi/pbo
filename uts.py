f0281b14668a903f8775433bcaf41af10bb7fa13
Rijal <56143547+rijalrpl12345@users.noreply.github.com>
2023-11-15 02:19:54 +0000

    Jawaban UTS Rijal MUbarok A

--- uts.py
+++ uts.py
@@ -1,5 +1,22 @@
-#!/usr/bin/env python3
+class Buku:
+    def __init__(self, judul, penulis, harga):
+        self.judul = judul
+        self.penulis = penulis
+        self.harga = harga
 
-# Copyright (c) Pipin Fitriadi. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license
-# information.
+    def tampilkan_info(self):
+        print(f"Informasi Buku:\nJudul: {self.judul}\nPenulis: {self.penulis}\nHarga: {self.harga}")
+
+# Contoh penggunaan class Buku
+buku_saya = Buku("Judul Buku Saya", "Penulis Saya", 100000)
+buku_saya.tampilkan_info()
+
+# Menambahkan 3 buku
+buku1 = Buku("Judul Buku 1", "Penulis 1", 75000)
+buku2 = Buku("Judul Buku 2", "Penulis 2", 90000)
+buku3 = Buku("Judul Buku 3", "Penulis 3", 120000)
+
+# Menampilkan informasi 3 buku yang ditambahkan
+buku1.tampilkan_info()
+buku2.tampilkan_info()
+buku3.tampilkan_info()
