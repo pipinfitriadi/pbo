689fc43ed923201784e1d752f0e872c4994057eb
rifkysalimlubis <128138905+rifkysalimlubis@users.noreply.github.com>
2023-11-15 02:09:14 +0000

    Jawaban UTS, Rifky Salim Lubis 0225013

--- uts.py
+++ uts.py
@@ -3,3 +3,16 @@
 # Copyright (c) Pipin Fitriadi. All rights reserved.
 # Licensed under the MIT License. See LICENSE in the project root for license
 # information.
+class Pesawat:
+    def __init__(self, nomor_keberangkatan, tujuan, waktu_keberangkatan):
+        self.nomor_keberangkatan = nomor_keberangkatan
+        self.tujuan = tujuan
+        self.waktu_keberangkatan = waktu_keberangkatan
+
+    def tampil_atribut(self):
+        print("Nomor Keberangkatan: ", self.nomor_keberangkatan)
+        print("Tujuan: ", self.tujuan)
+        print("Waktu_Keberangkatan: ", self.waktu_keberangkatan) 
+
+p1 = Pesawat("PK-123", "Jakarta", "06:00")
+p1.tampil_atribut()
