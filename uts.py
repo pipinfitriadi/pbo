83b6811619b69cca70d31a6b2314d75da3a5a0ad
Rijal <56143547+rijalrpl12345@users.noreply.github.com>
2023-11-15 02:21:15 +0000

    Jawaban UTS Rijal Mubarok a

--- uts.py
+++ uts.py
@@ -7,16 +7,16 @@ class Buku:
     def tampilkan_info(self):
         print(f"Informasi Buku:\nJudul: {self.judul}\nPenulis: {self.penulis}\nHarga: {self.harga}")
 
-# Contoh penggunaan class Buku
+# menjalankan fungsi clas buku
 buku_saya = Buku("Judul Buku Saya", "Penulis Saya", 100000)
 buku_saya.tampilkan_info()
 
-# Menambahkan 3 buku
+# daftar buku yang ada
 buku1 = Buku("Judul Buku 1", "Penulis 1", 75000)
 buku2 = Buku("Judul Buku 2", "Penulis 2", 90000)
 buku3 = Buku("Judul Buku 3", "Penulis 3", 120000)
 
-# Menampilkan informasi 3 buku yang ditambahkan
+#menampilkan list buku yang ada
 buku1.tampilkan_info()
 buku2.tampilkan_info()
 buku3.tampilkan_info()
