cf647a62449f863996fafbb86602055ddd313a6b
hendicprn <147573274+hendicprn@users.noreply.github.com>
2023-11-15 01:36:24 +0000

    submit Awal UTS

--- uts.py
+++ uts.py
@@ -1,5 +1,22 @@
-#!/usr/bin/env python3
+class pesawat:
 
-# Copyright (c) Pipin Fitriadi. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license
-# information.
+    def __init__ (self, NomorKeberangkatan, Tujuan, WaktuKeberangkatan):
+        self.NomorKeberangkatan = NomorKeberangkatan
+        self.Tujuan = Tujuan
+        self.WaktuKeberangkatan = WaktuKeberangkatan
+
+    def berangkat(self):
+        print("Nomor Keberangkataan pesawat Ini adalah :", self.NomorKeberangkatan)
+
+    def menuju(self):
+        print("Tujuan Pesawat Ini Menuju :", self.Tujuan)
+
+    def waktu(self):
+        print("Waktu keberangkatan pesawaat ini Pukul :", self.WaktuKeberangkatan)
+
+naikpesawat = pesawat(1, 2, 3)
+
+naikpesawat.berangkat()
+naikpesawat.menuju()
+naikpesawat.waktu()
+print(naikpesawat.NomorKeberangkatan, naikpesawat.Tujuan, naikpesawat.WaktuKeberangkatan)
