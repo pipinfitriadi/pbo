4cbc9317ea0db7d482395bac0b184a4e092609c0
Noval Rizky <115945247+noval5html@users.noreply.github.com>
2023-11-15 01:56:03 +0000

    Jawaban_UTS_NovalRizky_0225010

--- uts.py
+++ uts.py
@@ -1,5 +1,24 @@
-#!/usr/bin/env python3
+import math
 
-# Copyright (c) Pipin Fitriadi. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license
-# information.
+
+class Lingkaran:
+
+    def __init__(self, jari_jari):
+        self.jari_jari = jari_jari
+
+    def hitung_luas(self):
+        return math.pi * self.jari_jari ** 2
+
+    def hitung_keliling(self):
+        return 2 * math.pi * self.jari_jari
+
+
+# Contoh penggunaan
+jari_jari_lingkaran = float(input("Masukkan jari-jari lingkaran: "))
+lingkaran = Lingkaran(jari_jari_lingkaran)
+
+luas = lingkaran.hitung_luas()
+keliling = lingkaran.hitung_keliling()
+
+print(f"Luas lingkaran: {luas:.2f}")
+print(f"Keliling lingkaran: {keliling:.2f}")
