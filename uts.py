76194f02fd9c79b8b4c934d7a28e2baacef8d8bc
raihanmiftah5 <147573422+raihanmiftah5@users.noreply.github.com>
2023-11-15 01:51:25 +0000

    Jawaban_UTS_Muhammad_Raihan

--- uts.py
+++ uts.py
@@ -7,5 +7,6 @@ class Kendaraan:
 mobil = Kendaraan(max_speed=200, mileage=15)
 motor = Kendaraan(max_speed=150, mileage=20)
 
+
 print(f"Mobil - Maksimal Kecepatan: {mobil.max_speed} km/jam, Jarak Tempuh: {mobil.mileage} km")
 print(f"Motor - Maksimal Kecepatan: {motor.max_speed} km/jam, Jarak Tempuh: {motor.mileage} km")
