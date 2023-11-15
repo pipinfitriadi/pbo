6cd307ca64a3351444126a7dcf55472f26e05e53
raihanmiftah5 <147573422+raihanmiftah5@users.noreply.github.com>
2023-11-15 01:36:38 +0000

    Jawaban_UTS

--- uts.py
+++ uts.py
@@ -1,5 +1,11 @@
-#!/usr/bin/env python3
+class Kendaraan:
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
 
-# Copyright (c) Pipin Fitriadi. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license
-# information.
+        
+mobil = Kendaraan(max_speed=200, mileage=15)
+motor = Kendaraan(max_speed=150, mileage=20)
+
+print(f"Mobil - Maksimal Kecepatan: {mobil.max_speed} km/jam, Jarak Tempuh: {mobil.mileage} km")
+print(f"Motor - Maksimal Kecepatan: {motor.max_speed} km/jam, Jarak Tempuh: {motor.mileage} km")
