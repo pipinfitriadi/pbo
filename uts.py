db946557602484478b035724577ee7aad731438b
Windasukmawati <147573380+Windasukmawati@users.noreply.github.com>
2023-11-15 01:47:52 +0000

    Jawaban UTS Winda Sukmawati

--- uts.py
+++ uts.py
@@ -6,9 +6,9 @@ class Buku:
 
     def informasibuku(self):
         print("====Informasi Buku====")
-        print("Judul Buku : ", {self.judul})
-        print("Penulis Buku : ", {self.penulis})
-        print("Harga Buku : ", {self.harga})
+        print("Judul Buku : ", self.judul)
+        print("Penulis Buku : ", self.penulis)
+        print("Harga Buku : ", self.harga)
 
 
 Mybook = Buku("Harry Potter", "J.K. Rowling", 150000)
