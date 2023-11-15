d5da890002b0141af7de292cade8ba8ed1c5b679
hendicprn <147573274+hendicprn@users.noreply.github.com>
2023-11-15 02:56:19 +0000

    Perbaikan UTS

--- uts.py
+++ uts.py
@@ -1,5 +1,5 @@
 class pesawat:
-    def __init__ (self, NomorKeberangkatan, Tujuan, WaktuKeberangkatan):
+    def __init__(self, NomorKeberangkatan, Tujuan, WaktuKeberangkatan):
         self.NomorKeberangkatan = NomorKeberangkatan
         self.Tujuan = Tujuan
         self.WaktuKeberangkatan = WaktuKeberangkatan
@@ -8,12 +8,13 @@ class pesawat:
         print("Nomor Keberangkataan pesawat Ini adalah :", self.NomorKeberangkatan)
 
     def menuju(self):
-        print("Tujuan Pesawat Ini Menuju :", self.Tujuan )
+        print("Tujuan Pesawat Ini Menuju :", self.Tujuan)
 
     def waktu(self):
-        print("Waktu keberangkatan pesawaat ini Pukul :", self.WaktuKeberangkatan , "PM")
+        print("Waktu keberangkatan pesawaat ini Pukul :", self.WaktuKeberangkatan, "PM")
 
-naikpesawat = pesawat( "IP123", "Paris", "12.00")
+
+naikpesawat = pesawat("IP123", "Paris", "12.00")
 
 naikpesawat.berangkat()
 naikpesawat.menuju()
