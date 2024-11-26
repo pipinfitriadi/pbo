af4db32ee8949fb20fea2b216e7c954d19a13342
fauzisimatupang889 <ahmadfauzisimatupang889@gmail.com>
2024-11-26 03:02:51 +0000

    fauzi

--- uts.py
+++ uts.py
@@ -1,6 +1,2 @@
-#!/usr/bin/env python3
 
-# Copyright (c) Pipin Fitriadi. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license
-# information.
-print("Dulu")
+print("UTS")
