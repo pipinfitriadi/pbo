bf4ce7c470486bc36fd23d42e4166f3eeee77118
Arrss111 <a27ya.7@gmail.com>
2024-12-01 02:38:34 +0000

    Percobaan 1 Arya

--- uts.py
+++ uts.py
@@ -1,1 +1,18 @@
-print('hello world')
+#  sistem manajemen portofolio investasi
+# test apakah bisa berubah langsung atau tidak pada UTSnya
+class investor:
+    def __init__(self):
+        pass
+
+
+class asset:
+    def __init__(self):
+        pass
+
+
+class portofolio:
+    investor: str
+
+
+class sistemInvestasi:
+    nama: str
