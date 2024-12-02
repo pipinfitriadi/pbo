0708aedcf6129b5b302a2154a0f1d775d53a58ae
nissyauliyantis <nissyauliyantis@gmail.com>
2024-12-02 07:29:48 +0000

    nissy

--- uts.py
+++ uts.py
@@ -1,3 +1,8 @@
-print  ("Nissy")
-print ("MBD")
-
+# enkapsulasi
+class Pasien:
+    def __init__(self, nama, nomor_identitas, usia):
+        self.__nama = nama
+        self.__nomor_identitas = nomor_identitas
+        self.__usia = usia
+    def tampilkan_info(self):
+        return f"pasien: {self.__nama}, ID: {self.__nomor_identitas}, Usia: {self.__usia}"
