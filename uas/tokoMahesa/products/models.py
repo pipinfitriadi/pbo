f0f813c8b3a972c34e786436d266ff325456f51f
Mahesa safta linggas s <mahesa67ls@gmail.com>
2025-01-20 22:49:46 +0700

    2

--- uas/tokoMahesa/products/models.py
+++ uas/tokoMahesa/products/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from djangoo.urls import reverse
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+    slug = models.SlugField(unique=true)
+    
+    
+    class Meta:
+        varbose_name_plural = "categories"
+    
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    category = models.Foreginkey(Category. related_name='products', on_delete=models.CASCADE) 
+    name = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    description = models.TextField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='/products', blank=True, null=True)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    def def get_absolute_url(self):
+        return reverse('products:product_detail', kwargs={'id':self.id, 'siug':self.slug})
+    
+    
+    
