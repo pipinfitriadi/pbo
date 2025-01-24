16c3ad786bd54d8e0a44684407efcb3378e697ae
Daniswara Aryaguna <a27ya.7@gmail.com>
2025-01-24 22:42:32 +0700

    test database

--- uas/webarya/aryaapp/models.py
+++ uas/webarya/aryaapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class karyaSeni(models.Model):
+    Judul_Karya = models.CharField(max_length=128)
+    Nama_pembuat = models.CharField(max_length=64)
+    deskripsi = models.CharField(max_length=258)
+    harga = models.IntegerField()
+
+    # representasikan karyaSeni
+    def __str__(self):
+        return (f"ID: {self.id}: Dari {self.Judul_Karya} Dibuat oleh {self.Nama_pembuat} Deskripsi : {self.deskripsi}, dijual dengan harga Rp.{self.harga}")
