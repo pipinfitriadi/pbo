c56c2c71ddba9e18acd7e4189a9ec6e771a8b5a8
Daniswara Aryaguna <a27ya.7@gmail.com>
2025-01-26 14:05:04 +0700

    forms & contact

--- uas/webarya/aryaapp/urls.py
+++ uas/webarya/aryaapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import index
+from . import views
 
 # define a list urls pattern
 urlpatterns = [
-    path("", index, name= "index"),
+    path('', views.home_view, name='home'),
+    path('contact/', views.contact_view, name='contact'),
+    path('contact/success', views.contact_success_view,name='contact-success'),
 ]
--- uas/webarya/aryaapp/views.py
+++ uas/webarya/aryaapp/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
-from .models import karyaSeni
+from django.shortcuts import render, redirect
+from .form import ContactForm
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html')
 
+def home_view(request):
+    return render(request, 'aryaapp/home.html')
+
+# Define the contact_view function to handle contact form
+def contact_view(request):
+   if request.method == "POST":
+       form = ContactForm(request.POST)
+       if form.is_valid():
+           form.send_email()
+           return redirect('contact-success')
+   else:
+       form = ContactForm()
+   context = {'form':form}
+   return render(request, 'aryaapp/contact.html', context)
+
+# define the contact_success_view function to handle the success page
+def contact_success_view(request):
+    return render(request, 'aryaapp/contact_success.html')
