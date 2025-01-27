fc35723f8489830fb7f19e3cdd7ac97443d3481b
Daniswara Aryaguna <a27ya.7@gmail.com>
2025-01-27 10:41:54 +0700

    Mulai pengembangan

--- uas/webarya/aryaapp/static/js/script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('halaman berjalan, dan script berjalan');
-    alert("halo semua!")
-});
--- uas/webarya/webarya/settings.py
+++ uas/webarya/webarya/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# MEDIA_URL = 'images/'
+MEDIA_URL = 'images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
