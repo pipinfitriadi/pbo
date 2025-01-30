54509f3384330e328182e9424fbbabb17f6af112
Daniswara Aryaguna <a27ya.7@gmail.com>
2025-01-30 14:26:32 +0700

    register

--- uas/webarya/aryaapp/form.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    pesan = forms.CharField(widget=forms.Textarea)
-
-    def send_email(self):
-        print(f"email dari {self.cleaned_data['email']} dengan pesan: {self.cleaned_data['pesan']}")
