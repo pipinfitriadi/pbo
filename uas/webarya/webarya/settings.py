9899c23eca99ab017820b6e17582d85f7b8f9ae5
Daniswara Aryaguna <a27ya.7@gmail.com>
2025-01-30 20:30:16 +0700

    permulaan pembuatan keranjang

--- uas/webarya/webarya/settings.py
+++ uas/webarya/webarya/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'aryaapp.apps.AryaappConfig', 
+    'aryaapp.apps.AryaappConfig',
+    'cart' 
 ]
 
 MIDDLEWARE = [
